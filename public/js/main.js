83c63ff5e50fefda3338db72c5fa91a7922c7489
Alejandro Bologna <abologna@apples-macbook-pro-2.local>
2011-07-15 15:16:43 -0300

    get connections and filter on search input

--- public/js/main.js
+++ public/js/main.js
@@ -15,11 +15,35 @@ IN.Event.on(IN, 'systemReady', function() {
 
   // Sample listener.
   $(document).bind('input:changed', function(event, text) {
-    console.log(text);
+    onSearchInput(text);
   });
 });
 
+var connections = [];
+
 function _onAuth () {
+  getConnections(_onConnectionsReady);
+}
+
+function _onConnectionsReady() {
   $('#input').attr('disabled', false).attr('placeholder', 'Search your network...');
   $('#li-login').hide();
+}
+
+function getConnections(callback) {
+  $.getJSON('connections.pablo.json', function(data) {
+    connections = data.connections;
+    callback();
+  });  
+}
+
+function onSearchInput(text) {
+  var filtered = _.select(connections, function(connection) {
+    var include = false;
+    if (connection.industry) {
+      include = connection.industry.substring(0, text.length) === text;
+    }
+    return include;
+  });
+  console.log(filtered);
 }

d8b8e561fa3eeb66ef19cbbdb26e4a2bad4a9354
Pablo Fernandez <fernandezpablo85@gmail.com>
2011-07-15 12:40:58 -0300

    login functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,16 @@
-IN.Event.on(IN, "auth", function () {
-  
+IN.Event.on(IN, 'systemReady', function() {
+  if (IN.User.isAuthorized()) {
+    _onAuth();
+  } else {
+    $('#input').attr('placeholder', 'You must login in order to use InSpeed');
+    $('#li-login').show();
+    $('#li-login').click(function () {
+      IN.User.authorize(_onAuth);
+    });
+  }
 });
+
+function _onAuth () {
+  $('#input').attr('disabled', false).attr('placeholder', 'Search your network...');
+  $('#li-login').hide();
+}
